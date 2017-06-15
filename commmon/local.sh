717923d306c4f013f51c7b08d64644fb208a0858
Antonio <antonio.arena@kaust.edu.sa>
2017-06-16 00:08:05 +0300

    Typo fix.

--- commmon/local.sh
+++ commmon/local.sh
@@ -1,12 +1,14 @@
 export MANPATH=:
 
+export KAUST_CLUSTER=rws
+
 export KAUST_APPS_ROOT=/opt/share
 
 export KAUST_MODULES_ROOT=$KAUST_APPS_ROOT/modules
 
 rel=$(lsb_release -d | gawk -F"\t" '{print $2}')
 os=$([[ $rel == *"Ubuntu"* ]] && echo ubuntu || echo centos)
-# Order is important here, paths first in thew list have precedence
+# Order is important here, paths first in the list have precedence
 MODULEPATH=~/local/modulefiles:$KAUST_MODULES_ROOT/$os:$KAUST_MODULES_ROOT/applications
 MODULEPATH=$MODULEPATH:$KAUST_MODULES_ROOT/compilers:$KAUST_MODULES_ROOT/libs:$KAUST_MODULES_ROOT/sets
 export MODULEPATH
